2441f7f6bd1d97251533aab3de11f897b65bad98
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2025-07-06 14:00:36 -0300

    [fix] Corrigindo main

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# main.py
-
 from game.player import Jogador
 from game.engine import GameEngine
 
 def main():
-    # 1. Crie os jogadores que participarão da partida
     jogadores = [
         Jogador(nome="Fernando"),
         Jogador(nome="Maria"),
@@ -12,10 +9,8 @@ def main():
         Jogador(nome="Ana")
     ]
 
-    # 2. Crie o motor do jogo com a lista de jogadores
     motor_do_jogo = GameEngine(jogadores)
 
-    # 3. Rode a partida!
     motor_do_jogo.rodar_partida()
 
 
